a4f0ddd6581caf72f9c2831225554986b06450bb
fortuneanagor <chigoanagor@gmail.com>
2023-02-23 12:52:41 -0800

    0x04-more_functions_nested_loops0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * function - to check the uppercase
+ * 
+ */
+
+int _isupper(int c);
+{
+	if((c>= 'A') && (c<= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
