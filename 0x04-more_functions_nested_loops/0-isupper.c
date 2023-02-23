571e8b011500c21e3b2fcd04dd8f1d2c3941fc93
fortuneanagor <chigoanagor@gmail.com>
2023-02-23 13:01:00 -0800

    0x04-more_functions_nested_loops00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,12 +2,16 @@
 
 /**
  * function - to check the uppercase
- * 
+ * isupper - checks if the letters are uppercase
+ * @c: input
+ *
+ * Return: 1 if c is uppercase
+ * 0 otherwise
  */
 
 int _isupper(int c);
 {
-	if((c>= 'A') && (c<= 'Z'))
+	if ((c >= 'A') && (c <= 'Z'))
 	{
 		return (1);
 	}
