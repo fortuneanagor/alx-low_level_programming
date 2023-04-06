276d85511c1c0881f19861d4423847a2502ef541
fortuneanagor <chigoanagor@gmail.com>
2023-04-05 22:04:29 -0700

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,16 +1,11 @@
 #include "lists.h"
 
-
-
-
-
 /**
- * print_listint - prints all the elements inside the list.
- * @h
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
  *
  * Return: numbers of nodes.
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t nnodes = 0;
